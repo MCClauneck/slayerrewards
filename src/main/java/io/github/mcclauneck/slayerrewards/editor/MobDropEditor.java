5a9f90d8e8d6fe9c7a8ed948a7363e08213d9acf
JetsadaWijit <jetsadawijit@outlook.com>
2026-01-26 21:36:08 +0700

    fix: save page state before refreshing GUI in control button interactions

--- src/main/java/io/github/mcclauneck/slayerrewards/editor/MobDropEditor.java
+++ src/main/java/io/github/mcclauneck/slayerrewards/editor/MobDropEditor.java
@@ -193,10 +193,12 @@ public class MobDropEditor implements Listener {
                     Bukkit.getScheduler().runTask(plugin, () -> openEditor(player, session.mobName, session.page + 1));
                 } 
                 else if (event.getSlot() == 49) {
+                    EditorUtil.savePage(mobsFolder, session.mobName(), session.page(), event.getInventory());
                     EditorUtil.toggleDefaultDrops(mobsFolder, session.mobName());
                     Bukkit.getScheduler().runTask(plugin, () -> openEditor(player, session.mobName, session.page));
                 }
                 else if (event.getSlot() == 48 && session.page == 1) {
+                    EditorUtil.savePage(mobsFolder, session.mobName(), session.page(), event.getInventory());
                     cycleCurrency(session.mobName);
                     Bukkit.getScheduler().runTask(plugin, () -> openEditor(player, session.mobName, session.page));
                 }
